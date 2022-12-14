0afa7f79a64f1fb635e2d65fe353746fe15e86b7
Anurag <anuragsinghal6072@gmail.com>
2022-12-14 15:16:14 +0530

    first commit

---
+++ game.js
@@ -0,0 +1,81 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var userClickedPattern = [];
+var gamePattern = [];
+var level = 0;
+var started = false;
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").on("click", function () {
+  var userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function nextSequence() 
+{
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) 
+{
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) 
+    {
+      console.log("success");
+      if (userClickedPattern.length === gamePattern.length)
+      {
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+
+    } 
+    else
+    {
+        var audio = new Audio("sounds/wrong.mp3");
+        audio.play();
+
+        $("body").addClass("game-over");
+        setTimeout(function()
+        {
+            $("body").removeClass("game-over");
+        },200);
+
+        $("h1").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+}
+
+function startOver()
+{
+    gamePattern = [];
+    level = 0;
+    started = false;
+}
